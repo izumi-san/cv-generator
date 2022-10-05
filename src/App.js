fb4d196c604fbadbe133f49fe31b4010550d9d30
Cassio Izumi <izumicassio@gmail.com>
2022-10-05 19:14:12 -0300

    form personal info section rededigned, changed color and hide fileds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import FormContainer from './components/FormContainer'
+import FormContainer from './components/FormContainer';
 
 function App() {
-  return <div className='App'>
-  <FormContainer/>
-  </div>;
+  return (
+    <div className='App'>
+      <FormContainer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/FormContainer/components/PersonalInfoSection/index.tsx
+++ src/components/FormContainer/components/PersonalInfoSection/index.tsx
@@ -1,4 +1,5 @@
 import CustomInput from './../../CustomInput';
+import style from './PersonalInfoSection.module.scss';
 
 interface Props {
   formValues: {
@@ -15,7 +16,7 @@ interface Props {
 
 function PersonalInfoSection(props: Props) {
   return (
-    <section>
+    <section className={style.container}>
       <h2>Personal Info</h2>
       <CustomInput
         name={'firstName'}
@@ -25,54 +26,73 @@ function PersonalInfoSection(props: Props) {
         label={'First Name'}
         onChange={props.handleInputs}
       />
-      <CustomInput
-        name={'lastName'}
-        value={props.formValues.lastName}
-        placeholder={''}
-        type={'text'}
-        label={'Last Name'}
-        onChange={props.handleInputs}
-      />
-      <CustomInput
-        name={'title'}
-        value={props.formValues.title}
-        placeholder={''}
-        type={'text'}
-        label={'Title'}
-        onChange={props.handleInputs}
-      />
-      <CustomInput
-        name={'cep'}
-        value={props.formValues.cep}
-        placeholder={''}
-        type={'text'}
-        label={'CEP'}
-        onChange={props.handleInputs}
-      />
-      <CustomInput
-        name={'phoneNumber'}
-        value={props.formValues.phoneNumber}
-        placeholder={''}
-        type={'text'}
-        label={'Phone Number'}
-        onChange={props.handleInputs}
-      />
-      <CustomInput
-        name={'email'}
-        value={props.formValues.email}
-        placeholder={''}
-        type={'text'}
-        label={'email'}
-        onChange={props.handleInputs}
-      />
-      <CustomInput
-        name={'linkedIn'}
-        value={props.formValues.linkedIn}
-        placeholder={''}
-        type={'text'}
-        label={'linkedIn'}
-        onChange={props.handleInputs}
-      />
+      {/*
+      <div className={style.containerFields}>
+        <CustomInput
+          name={'lastName'}
+          value={props.formValues.lastName}
+          placeholder={''}
+          type={'text'}
+          label={'Last Name'}
+          onChange={props.handleInputs}
+        />
+      </div>
+        */}
+      <div className={style.containerFields}>
+        <CustomInput
+          name={'title'}
+          value={props.formValues.title}
+          placeholder={''}
+          type={'text'}
+          label={'Title'}
+          onChange={props.handleInputs}
+        />
+        <CustomInput
+          name={'email'}
+          value={props.formValues.email}
+          placeholder={''}
+          type={'text'}
+          label={'email'}
+          onChange={props.handleInputs}
+        />
+      </div>
+      {/* 
+        <CustomInput
+          name={'email'}
+          value={props.formValues.email}
+          placeholder={''}
+          type={'text'}
+          label={'email'}
+          onChange={props.handleInputs}
+        />
+        <CustomInput
+          name={'linkedIn'}
+          value={props.formValues.linkedIn}
+          placeholder={''}
+          type={'text'}
+          label={'linkedIn'}
+          onChange={props.handleInputs}
+        />
+
+      <div className={style.containerFields}>
+        <CustomInput
+          name={'phoneNumber'}
+          value={props.formValues.phoneNumber}
+          placeholder={''}
+          type={'text'}
+          label={'Phone Number'}
+          onChange={props.handleInputs}
+        />
+        <CustomInput
+          name={'cep'}
+          value={props.formValues.cep}
+          placeholder={''}
+          type={'text'}
+          label={'CEP'}
+          onChange={props.handleInputs}
+        />
+      </div>
+      */}
     </section>
   );
 }
